03763f5a85df80bfcb433397bc50923e93b1d230
Joshua Marangoni <joshua.marangoni@gmail.com>
2023-01-08 21:02:09 -0800

    add custom delay

--- atmega.ino
+++ atmega.ino
@@ -1,16 +1,25 @@
 
-#define CUSTOM_DDRB  *((volatile byte*)0x24)
-#define CUSTOM_PORTB *((volatile byte*)0x25)
+#define CUSTOM_DDRB  *((volatile unsigned char*)0x24)
+#define CUSTOM_PORTB *((volatile unsigned char*)0x25)
+
+#define PERIOD 2000000U
+
+void custom_delay(long int n, bool on)
+{
+  for (long i = 0; i < n; i++) {
+    CUSTOM_PORTB = on ? (1 << 5) : 0;
+  }
+}
 
 void setup() {
   // set PB5 as a digital output
-  CUSTOM_DDRB = 32;
+  CUSTOM_DDRB = (1 << 5);
 }
 
 void loop() {
-  // turn on PB5 for 1M clock cyles
-  for (long i = 0; i < 1000000; i++) { CUSTOM_PORTB = 32 ;}
-
-  // turn off PB5 for 1M clock cyles
-  for (long i = 0; i < 1000000; i++) { CUSTOM_PORTB = 0 ;}
+  // turn on PB5 register for certain number of clock cycles
+  custom_delay(PERIOD, false);
+ 
+  // turn on PB5 certain number of clock cycles
+  custom_delay(PERIOD, true);
 }
